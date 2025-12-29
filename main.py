24c51084d67831d8eb3fceb833a43b4ae3eae1b5
Агеева Софья <son4a1996@yandex.ru>
2025-12-29 19:41:29 +0300

    Input data for filter_by_state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card, get_date
+from src.processing import filter_by_state
 
 
 if __name__ == "__main__":
@@ -7,9 +8,16 @@ if __name__ == "__main__":
     print(masked_card)
     masked_account = get_mask_account("73654108430135874305")
     print(masked_account)
+
     masked_data = mask_account_card("Visa Platinum 7000792289606361")
     print(masked_data)
     masked_data = mask_account_card("Счет 73654108430135874305")
     print(masked_data)
     formated_date = get_date("2024-03-11T02:26:18.671407")
     print(formated_date)
+
+    new_state = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    print(new_state)

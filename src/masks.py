b9c8fe7989505f65634a0db9b21e063088e736ef
Агеева Софья <son4a1996@yandex.ru>
2026-02-02 18:51:32 +0300

    Доработана функция get_mask_card_number

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,11 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция шифрования номера карты пользователя"""
+    # Очищаем номер от пробелов и дефисов
+    cleaned = card_number.replace(" ", "").replace("-", "")
+    if len(cleaned) < 16:
+        return card_number
     # Зашифровываем символы карты (6-12)
-    mask_card_number = card_number[:6] + "******" + card_number[12:]
+    mask_card_number = cleaned[:6] + "**" + "****" + cleaned[-4:]
     # Разбиваем номер карты на блоки по 4 символа
     block_list = []
     for i in range(0, len(mask_card_number), 4):

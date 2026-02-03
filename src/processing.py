2bf5faac9dd2d201da9cecce4d37028ec037578b
Агеева Софья <son4a1996@yandex.ru>
2026-02-03 20:20:27 +0300

    Fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -14,4 +14,7 @@ def filter_by_state(filterable_state: List[Dict[str, Any]], state: str = "EXECUT
 
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Сортирует список словарей по дате"""
+    for item in data:
+        if "date" not in item:
+            raise KeyError("Отсутствует ключ 'date'")
     return sorted(data, key=lambda x: x["date"], reverse=reverse)

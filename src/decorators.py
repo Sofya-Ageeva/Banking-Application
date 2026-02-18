34bd812162a7eed48903bdb46075b7e15e983fde
Агеева Софья <son4a1996@yandex.ru>
2026-02-18 18:40:41 +0300

    Реализован декоратор log с возможностью вывода логов в файл

--- src/decorators.py
+++ src/decorators.py
@@ -1,22 +1,40 @@
-from time import time
+from datetime import datetime
 from functools import wraps
 
 
 def log(filename = None):
     def wrapper(func):
         @wraps(func)
-        def inner (*args, **kwargs):
-            start_func = time()
-            result = func(*args, **kwargs)
-            end_time = time()
+        def inner(*args, **kwargs):
+            formatted_time = datetime.now()
+            start_time = formatted_time.strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                result = func(*args, **kwargs)
+                success_message = (f"{start_time}\n{func.__name__} ok "
+                           f"Результат: {result}")
 
-
-
-
+                if filename:
+                    with open(filename, 'w', encoding='utf-8') as f:
+                        f.write(success_message)
+                else:
+                    print(f"{func.__name__} ok Результат: {result}")
+                return result
+            except Exception as e:
+                error_message = (f"{start_time}\n{func.__name__} "
+                                 f"error: {type(e).__name__}."
+                                 f"Inputs: ({args}, {kwargs})")
+                if filename:
+                    with open(filename, 'w', encoding='utf-8') as f:
+                        f.write(error_message)
+                else:
+                    print(f"{func.__name__} error: {type(e).__name__}."
+                      f"Inputs: ({args}, {kwargs})")
+        return inner
+    return wrapper
 
 
 @log(filename="mylog.txt")
 def my_function(x, y):
     return x + y
 
-my_function(1, 2)
+my_function("f", 2)

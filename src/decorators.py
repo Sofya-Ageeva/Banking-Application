de578abe97d61a3230d3fd0dcde6af72edd2ae94
Агеева Софья <son4a1996@yandex.ru>
2026-02-19 19:35:12 +0300

    Decorators fixes

--- src/decorators.py
+++ src/decorators.py
@@ -1,19 +1,23 @@
 from datetime import datetime
 from functools import wraps
+from typing import Callable, Any, Optional
 
 
-def log(filename = None):
+def log(filename: Optional[str] = None) -> Callable[..., Any]:
     """Выводит результат выполнения функции в файл, если такое значение задано по умолчанию"""
-    def wrapper(func):
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            """Внутренняя функция декоратора.
+            Выполняет логирование до и после вызова декорируемой функции.
+            """
             formatted_time = datetime.now()
             start_time = formatted_time.strftime("%Y-%m-%d %H:%M:%S")
             end_time = formatted_time.strftime("%Y-%m-%d %H:%M:%S")
             try:
                 result = func(*args, **kwargs)
                 success_message = (f"{start_time}\n{func.__name__} ok "
-                           f"Результат: {result}"
+                                   f"Результат: {result}"
                                    f"\n{end_time}")
 
                 if filename:
@@ -31,9 +35,16 @@ def log(filename = None):
                     with open(filename, 'w', encoding='utf-8') as f:
                         f.write(error_message)
                 else:
-                    print(f"{func.__name__} error: {type(e).__name__}."
-                      f"Inputs: {args}, {kwargs}")
-
+                    print(error_message)
                 raise
         return inner
     return wrapper
+
+
+if __name__ == "__main__":
+    @log(filename="mylog.txt")
+    def my_function(x: int, y: int) -> int:
+        """Возвращает сумму"""
+        return x + y
+
+    my_function("s", 2)

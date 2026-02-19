e6cbfc416464f73e715adc9953e3230107d23108
Агеева Софья <son4a1996@yandex.ru>
2026-02-19 17:59:25 +0300

    Decoratots.py fixes

--- src/decorators.py
+++ src/decorators.py
@@ -3,15 +3,18 @@ from functools import wraps
 
 
 def log(filename = None):
+    """Выводит результат выполнения функции в файл, если такое значение задано по умолчанию"""
     def wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
             formatted_time = datetime.now()
             start_time = formatted_time.strftime("%Y-%m-%d %H:%M:%S")
+            end_time = formatted_time.strftime("%Y-%m-%d %H:%M:%S")
             try:
                 result = func(*args, **kwargs)
                 success_message = (f"{start_time}\n{func.__name__} ok "
-                           f"Результат: {result}")
+                           f"Результат: {result}"
+                                   f"\n{end_time}")
 
                 if filename:
                     with open(filename, 'w', encoding='utf-8') as f:
@@ -22,19 +25,15 @@ def log(filename = None):
             except Exception as e:
                 error_message = (f"{start_time}\n{func.__name__} "
                                  f"error: {type(e).__name__}."
-                                 f"Inputs: ({args}, {kwargs})")
+                                 f"Inputs: {args}, {kwargs}"
+                                 f"\n{end_time}")
                 if filename:
                     with open(filename, 'w', encoding='utf-8') as f:
                         f.write(error_message)
                 else:
                     print(f"{func.__name__} error: {type(e).__name__}."
-                      f"Inputs: ({args}, {kwargs})")
+                      f"Inputs: {args}, {kwargs}")
+
+                raise
         return inner
     return wrapper
-
-
-@log(filename="mylog.txt")
-def my_function(x, y):
-    return x + y
-
-my_function("f", 2)

ad4c9e213b4cd942ddc8e327b2edba5d03b5789a
Агеева Софья <son4a1996@yandex.ru>
2026-02-26 18:20:12 +0300

    Реаализована функция обращения к файлу с данными

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+import os
+from typing import List, Dict
+
+
+def open_json_file(file_path: str) -> List[Dict]:
+    """Функция принимает на вход путь до файла с данными о транзакциях
+    и возвращает список словарей"""
+    if not os.path.exists(file_path):
+        return []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            received_data = json.load(f)
+            if len(received_data) != 0 and isinstance(received_data, List):
+                return received_data
+            else:
+                return []
+    except (json.JSONDecodeError, UnicodeDecodeError, PermissionError):
+        return []
+
+test = open_json_file('data/operations.json')
+print(test)

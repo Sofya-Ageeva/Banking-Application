f7994c32fcc3caeb9f58ccad9e7f709b9ba635ac
Агеева Софья <son4a1996@yandex.ru>
2026-03-12 17:48:20 +0300

    file fixes flake8 and mypy

--- src/read_data.py
+++ src/read_data.py
@@ -1,27 +1,29 @@
-import pandas as pd
-from typing import List, Dict, Any
+from typing import Any, Dict, Hashable, List
 
-def read_transaction_from_csv(file_path: str) ->List[Dict[str, Any]]:
+import pandas as pd
+
+
+def read_transaction_from_csv(file_path: str) -> List[Dict[Hashable, Any]]:
     """Чтение данных о транзакциях из файла формата .csv"""
     try:
         df = pd.read_csv(file_path)
-        if df.empy:
+        if df.empty:
             raise pd.errors.EmptyDataError('Отсутствуют данные для чтения')
         return df.to_dict('records')
     except FileNotFoundError:
         raise FileNotFoundError(f"Файл не найден: {file_path}")
-    except pd.errors.EmptyDataError as f:
-        raise pd.errors.EmptyDataError(f"Ошибка чтения файла: {f}")
+    except pd.errors.EmptyDataError:
+        raise
     except Exception as f:
         raise Exception(f"Ошибка при чтении файла: {f}")
 
 
-def read_transaction_from_excel(file_path: str) -> List[Dict[str, Any]]:
+def read_transaction_from_excel(file_path: str) -> List[Dict[Hashable, Any]]:
     """Чтение данных о транзакциях из файла формата .xlsx"""
     try:
         df = pd.read_excel(file_path)
-        if df.empy:
-            raise  ValueError("Отсутствуют данные для чтения")
+        if df.empty:
+            raise ValueError("Отсутствуют данные для чтения")
         return df.to_dict('records')
     except FileNotFoundError:
         raise FileNotFoundError(f"Файл не найден: {file_path}")
@@ -29,6 +31,3 @@ def read_transaction_from_excel(file_path: str) -> List[Dict[str, Any]]:
         raise ValueError(f"Ошибка чтения файла: {f}")
     except Exception as f:
         raise Exception(f"Ошибка при чтении файла: {f}")
-
-
-
